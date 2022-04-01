102fba38add28bc50812d8c83941e141b5a0cd88
Misha Samoylov <mikhailsamoylov95@gmail.com>
2022-04-01 07:55:56 +0000

    Added controller with showing and adding products

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -1,14 +1,52 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ProductsController
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+// use Symfony\Component\Validator\Validation;
+
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class ProductsController extends AbstractController
 {
-    public function show(): Response
+    public function show(ProductRepository $productRepository): Response
     {
-        return new Response(
-            '<html><body>123</body></html>'
-        );
+        $product = $productRepository
+            ->findAll();
+
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers);
+        $data = $serializer->normalize($product, null);
+
+        $response = new JsonResponse(['success' => true, 'products' => $data]);
+        return $response;
+    }
+
+    public function add(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        // TODO: Validate request
+
+        $parameters = json_decode($request->getContent(), true);
+        $name = $parameters['name'];
+        
+        $product = new Product();
+        $product->setName($name);
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        $response = new JsonResponse(['success' => true]);
+        return $response;
     }
 }
